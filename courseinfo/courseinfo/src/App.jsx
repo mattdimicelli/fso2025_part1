eba8cbe5fede43e3ff7e9012fc7b15f3da5d7303
Matthew Di Micelli <matthewdimicelli@gmail.com>
2024-12-08 13:06:03 -0500

    finish 1.5

--- courseinfo/courseinfo/src/App.jsx
+++ courseinfo/courseinfo/src/App.jsx
@@ -1,19 +1,21 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   const Header = (props) => (
     <h1>{props.course}</h1>
@@ -34,14 +36,14 @@ const App = () => {
   )
 
   const Total = (props) => (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts} />
-      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+      <Header course={course.name}/>
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
